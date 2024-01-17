67cf1004ac1df5501878b8ef5434615a1cd567ec
Ivan Štefanjak <ivan.stefanjak@kodilion.hr>
2024-01-17 02:58:55 +0100

    Iban regex removed

--- src/main/java/co/leapwise/banking/common/Properties.java
+++ src/main/java/co/leapwise/banking/common/Properties.java
@@ -3,5 +3,4 @@ package co.leapwise.banking.common;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "co.leapwise.banking")
-public record Properties(
-    String ibanRegex, Integer numberOfTransactions, String generatedFileName) {}
+public record Properties(Integer numberOfTransactions, String generatedFileName) {}
