e7eccd769d8ab63223758edb321ee5239d2977de
Ivan Štefanjak <ivan.stefanjak@kodilion.hr>
2024-01-23 18:04:37 +0100

    Spring batch test

--- src/main/java/co/leapwise/banking/manager/AccountManager.java
+++ src/main/java/co/leapwise/banking/manager/AccountManager.java
@@ -3,6 +3,7 @@ package co.leapwise.banking.manager;
 import co.leapwise.banking.model.Account;
 import co.leapwise.banking.repository.AccountRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,12 @@ public class AccountManager {
     return repository
         .findById(accountId)
         .orElseThrow(() -> new EntityNotFoundException("Account not found. ID: " + accountId));
+  }
+
+  public List<Account> getAll() {
+    var ret = new ArrayList<Account>();
+    repository.findAll().forEach(ret::add);
+    return ret;
   }
 
   public void insertAccount(Account account) {
